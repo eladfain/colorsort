a5fff48bf6f1d6e1a71a316384a78f9a020636be
eladfain <62603008+eladfain@users.noreply.github.com>
2023-05-20 20:25:46 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,109 @@
+let selected='';
+const colors=['red','blue','green','black','yellow','blueviolet','aqua','brown'];
+function start(){
+    document.querySelectorAll('.tube').forEach(tube=>tube.innerHTML='');
+    const colorArr=getRandomColorArr(),
+    tubecontainer=document.querySelector('.tubes');
+    tubecontainer.innerHTML='';
+    for(let i=0;i<(colorArr.length+2);i++){
+        const tube=document.createElement('div');
+        tube.classList.add('tube');
+        tube.id=`t${i}`;
+        tubecontainer.appendChild(tube);
+    }
+    displayBalls(colorArr);
+    document.querySelectorAll('.tube').forEach(tube=>tube.addEventListener('click',e=>tubeClick(e),false));
+}
+function displayBalls(arr){
+    const tubes=document.querySelectorAll('.tube');
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            const ball=getColorBall(arr[i][j]);
+            tubes[i].appendChild(ball)
+        }
+    }
+}
+function getColorBall(color){
+    const colorElm=document.createElement('span');
+    colorElm.classList.add('ball');
+    colorElm.style.backgroundColor=color;
+    colorElm.setAttribute('value',color)
+    return colorElm;
+}
+function getRandomColorArr(){
+    const numberOfColors=document.getElementById('colorNumber').value;
+    const reducedColorsArr=colors.slice(colors.length-numberOfColors)
+    const colorObj=reducedColorsArr.reduce((a,c)=>{
+        a[c]=4;
+        return a;   
+    },{}),
+    colorArr=[];
+    let keys=Object.keys(colorObj);
+    
+    colorArr.push(new Array());
+    while(keys.length){
+        const color=keys[Math.floor(Math.random()*keys.length)];
+        colorObj[color]=colorObj[color]-1;
+        if(colorObj[color]===0){
+            delete colorObj[color];
+            keys=Object.keys(colorObj)
+        }
+        if(colorArr[colorArr.length-1].length<4){
+            colorArr[colorArr.length-1].push(color);
+        }else{
+            colorArr.push(new Array());
+            colorArr[colorArr.length-1].push(color);
+        }
+    }
+    return colorArr;
+}
+function tubeClick(e){
+    const id=(e.target.id || e.target.parentNode.id);
+    if(!selected){
+        selected=id;
+        document.getElementById(selected).classList.add('selected');
+    }else{
+        const originTube=document.getElementById(selected);
+        const targetTube=document.getElementById(id);
+        const ballToMove=originTube.children[0];
+        const firstBallOnTarget=targetTube.children[0]
+        if((targetTube.children.length<4 && 
+            ballToMove.getAttribute('value')===firstBallOnTarget?.getAttribute('value')) ||
+            targetTube.children.length===0){
+                originTube.removeChild(ballToMove);
+                targetTube.prepend(ballToMove);
+                checkFinishedTube(targetTube);
+                checkWin();
+            }
+        document.getElementById(selected).classList.remove('selected');
+        
+        selected='';
+    }
+}
+function checkFinishedTube(tube){
+    let allTheSame=true;
+    const children=Array.prototype.slice.call(tube.children);
+    children.forEach(tube=>{
+        if(children[0].getAttribute('value')!==tube.getAttribute('value')){
+            allTheSame=false;
+        }
+    })
+    if(children.length===4 && allTheSame){
+        tube.classList.add('finised');
+    }
+}
+function checkWin(){
+    const finishedTubes=document.querySelectorAll(".finised");
+    if(finishedTubes.length===colors.length){
+        finishedTubes.forEach(tube=>{
+            tube.classList.remove('finised');
+            tube.classList.add('win')
+    })
+    }
+}
+function restart(){
+
+}
+function undo(){
+
+}
